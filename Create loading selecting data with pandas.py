66bba5358a167de93ddc8f40bf750aa3aee9bb99
iamchristinelee <thesooyunlee@gmail.com>
2018-04-13 14:11:30 +1000

    test

---
+++ Create loading selecting data with pandas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 05 11:18:57 2018
+
+@author: 126215
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#df=pd.read_csv('c:\_$\sample.csv')
+#print(df)
+
+df = pd.read_csv('c:\_$\imdb.csv')
+print(df)
+print(df.head())
+print(df.info())
+
+#select column
