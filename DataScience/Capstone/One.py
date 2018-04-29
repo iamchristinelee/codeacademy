2b5f5f8f5edd11282a20920a81f6fefc332c5265
iamchristinelee <thesooyunlee@gmail.com>
2018-04-29 10:06:55 +1000

    upload

--- DataScience/Capstone/One.py
+++ DataScience/Capstone/One.py
@@ -12,3 +12,44 @@ species_type = species.category.unique()
 print species_type
 conservation_statuses = species.conservation_status.unique()
 print conservation_statuses
+
+conservation_counts=species.groupby('conservation_status').scientific_name.nunique().reset_index()
+
+print conservation_counts
+
+species.fillna('No Intervention', inplace = True)
+
+
+conservation_counts_fixed=species.groupby('conservation_status').scientific_name.nunique().reset_index()
+print conservation_counts_fixed
+
+
+species = pd.read_csv('species_info.csv')
+
+species.fillna('No Intervention', inplace = True)
+
+protection_counts= species.groupby('conservation_status')\
+    .scientific_name.nunique().reset_index()\
+    .sort_values(by='scientific_name')
+    
+print protection_counts
+"""Create an axes objected called ax using plt.subplot.
+Create a bar chart whose heights are equal to the scientific_name column of protection_counts.
+Create an x-tick for each of the bars.
+Label each x-tick with the label from conservation_status in protection_counts.
+Label the y-axis Number of Species.
+Title the graph Conservation Status by Species
+Plot the graph using plt.show().
+"""
+plt.figure(figsize=(10,4))
+ax = plt.subplot()
+plt.bar(range(len(protection_counts.conservation_status)), protection_counts.scientific_name)
+ax.set_xticks(range(len(protection_counts.conservation_status)))
+ax.set_xticklabels(protection_counts.conservation_status)
+plt.ylabel('Number of Species')
+plt.title('Conservation Status by Species')
+plt.show()
+
+"""
+
+"""
